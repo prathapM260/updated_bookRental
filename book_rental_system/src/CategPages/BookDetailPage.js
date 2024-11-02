876dfd262fef965bb8e8c79ec66e5335cb0b3df5
prathapM260 <158457582+prathapM260@users.noreply.github.com>
2024-11-02 19:23:44 +0530

    Update BookDetailPage.js

--- book_rental_system/src/CategPages/BookDetailPage.js
+++ book_rental_system/src/CategPages/BookDetailPage.js
@@ -9,7 +9,7 @@ const NewArrivalBooks = () => {
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/books?category=Thriller');
+        const response = await axios.get('https://updated-bookrental-2.onrender.com/api/books?category=Thriller');
         setBooks(response.data);
       } catch (error) {
         console.error('Failed to fetch new arrival books:', error);
