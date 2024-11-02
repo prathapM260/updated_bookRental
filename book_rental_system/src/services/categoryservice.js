167e4f2c18b56b49ec97502285a4b3e69610477d
prathapM260 <158457582+prathapM260@users.noreply.github.com>
2024-11-02 19:57:39 +0530

    Update categoryservice.js

--- book_rental_system/src/services/categoryservice.js
+++ book_rental_system/src/services/categoryservice.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/categories';
+const API_URL = 'https://updated-bookrental-2.onrender.com/api/categories';
 
 export const fetchCategories = async () => {
   try {
